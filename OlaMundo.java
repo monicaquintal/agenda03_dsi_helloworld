00d7f80d60479dd1665a9893580e34ccbc06a22a
Mônica Zungalo Quintal <111545468+monicaquintal@users.noreply.github.com>
2022-09-19 20:04:51 -0300

    Add files via upload
    Primeiro programa em Java, realizado na Agenda 03 do Curso Técnico de Desenvolvimento de Sistemas da ETEC.

---
+++ OlaMundo.java
@@ -0,0 +1,11 @@
+package olamundo;
+
+public class OlaMundo {
+public static void main(String[] args) {
+        String meuPrimeiroPrograma;
+        meuPrimeiroPrograma = "Programa instalado com sucesso e em pleno funcionamento!";
+
+        System.out.println(meuPrimeiroPrograma);
+    }
+
+}
